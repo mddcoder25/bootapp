c0f6233449671e06a135f07974be20855ccfebce
mueller <mueller@se-rwth.de>
2018-01-14 14:02:23 +0100

    first commit test branch

--- src/main/java/de/test/boot/BootAppApplication.java
+++ src/main/java/de/test/boot/BootAppApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BootAppApplication {
 
+	/*
+	 * Test branch comment
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BootAppApplication.class, args);
 	}
